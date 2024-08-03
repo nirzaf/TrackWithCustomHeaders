5954d410be83b82781621b6dc705e5129415bb37
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-08-03 15:24:42 +0300

    feat: implement simple endpoint and extract data (master)
    Implement a simple endpoint that extracts custom header values
    'X-Correlation-ID' and 'X-Trace-ID' from the request and logs them
    using an ILogger.
    
    This commit also includes the following changes:
    
    - Add a new controller 'DataProcessController' to handle
      the new endpoint.
    - Create a new model 'ReferenceDto' to represent the extracted
      header values.
    - Update the 'TrackWithCustomHeaders.http' file with the new
      endpoint and custom headers.
    - Add a new folder 'Services' to the project.

---
+++ Controllers/DataProcessController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrackWithCustomHeaders.Models;
+
+namespace TrackWithCustomHeaders.Controllers;
+
+public class DataProcessController(ILogger<DataProcessController> logger) : Controller
+{
+    [HttpGet]
+    [Route("simpleDtoExtract")]
+    public IActionResult SimpleDtoExtract([FromHeader] ReferenceDto reference)
+    {
+        logger.LogInformation($"X-Correlation-ID: {reference.CorrelationId}");
+        logger.LogInformation($"X-Trace-ID: {reference.TraceId}");
+
+        return Ok(reference.CorrelationId);
+    }
+}
---
+++ Models/ReferenceDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrackWithCustomHeaders.Models;
+
+public class ReferenceDto
+{
+    [FromHeader(Name = "X-Correlation-ID")]
+    public string? CorrelationId { get; set; }
+
+
+    [FromHeader(Name = "X-Trace-ID")] public string? TraceId { get; set; }
+}
